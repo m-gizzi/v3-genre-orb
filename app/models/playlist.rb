b4d27c573f7267fb752d615d8cc02a589606d0e6
m-gizzi <mgizzi@eligoenergy.com>
2022-09-06 01:04:57 -0500

    Add more detailed error messages to validations

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,5 +4,5 @@ class Playlist < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates :playlist_id, presence: true
+  validates :playlist_id, presence: { message: "ID can't be blank" }, uniqueness: { message: 'ID has already been taken' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :api_logs, dependent: :destroy
 
   validates :full_name, presence: true
-  validates :spotify_id, presence: true, uniqueness: true
+  validates :spotify_id, presence: { message: "ID can't be blank" }, uniqueness: { message: 'ID has already been taken' }
 end
