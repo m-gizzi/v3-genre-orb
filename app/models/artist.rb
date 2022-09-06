8ad04ca3bf73660b697f40179630f17795594a2e
m-gizzi <mgizzi@eligoenergy.com>
2022-09-06 11:52:24 -0500

    Add Artist

---
+++ app/models/artist.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class Artist < ApplicationRecord
+  validates :name, presence: true
+  validates :spotify_id, presence: { message: "ID can't be blank" }, uniqueness: { message: 'ID has already been taken' }
+end
