62f34f42efbb1767c425714a6cc6a1224d9fb48e
m-gizzi <mgizzi@eligoenergy.com>
2022-09-26 12:02:48 -0500

    Actually use SpotifyClient class

--- lib/spotify_client.rb
+++ lib/spotify_client.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SpotifyClient
-  def self.get_playlist(playlist_id)
+  def self.get_rspotify_playlist_by_id(playlist_id)
     # rubocop:disable Rails/DynamicFindBy
     RSpotify::Playlist.find_by_id(playlist_id)
     # rubocop:enable Rails/DynamicFindBy
--- spec/lib/spotify_client_spec.rb
+++ spec/lib/spotify_client_spec.rb
@@ -4,15 +4,15 @@ require 'rails_helper'
 require 'spotify_client'
 
 describe SpotifyClient do
-  describe '.get_playlist' do
+  describe '.get_rspotify_playlist_by_id' do
     let(:playlist_id) { '3IlD894HSWDF8YlkCP25Sq' }
 
     it 'returns an RSpotify::Playlist', :vcr do
-      expect(described_class.get_playlist(playlist_id)).to be_a(RSpotify::Playlist)
+      expect(described_class.get_rspotify_playlist_by_id(playlist_id)).to be_a(RSpotify::Playlist)
     end
 
     it 'returns the correct playlist', :vcr do
-      expect(described_class.get_playlist(playlist_id).id).to eq playlist_id
+      expect(described_class.get_rspotify_playlist_by_id(playlist_id).id).to eq playlist_id
     end
   end
 end
