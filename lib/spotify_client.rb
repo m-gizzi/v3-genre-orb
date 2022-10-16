42dd82dad979c21bff8626dc31374a60befa129c
m-gizzi <mgizzi@eligoenergy.com>
2022-10-16 01:19:42 -0500

    Make SpotifyClient a class that gets instantiated

--- lib/spotify_client.rb
+++ lib/spotify_client.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SpotifyClient
-  def self.get_playlist_by_id(playlist_id)
+  def get_playlist_by_id(playlist_id)
     # rubocop:disable Rails/DynamicFindBy
     RSpotify::Playlist.find_by_id(playlist_id)
     # rubocop:enable Rails/DynamicFindBy
@@ -9,7 +9,7 @@ class SpotifyClient
 
   MAXIMUM_ARTIST_LOOKUPS_PER_CALL = 50 # RSpotify::Artist.find has a limit of 50 ids
 
-  def self.get_artists_by_ids(artist_ids)
+  def get_artists_by_ids(artist_ids)
     if artist_ids.count > MAXIMUM_ARTIST_LOOKUPS_PER_CALL
       raise ArgumentError, "#{__method__} cannot accept more than #{MAXIMUM_ARTIST_LOOKUPS_PER_CALL} ids as arguments"
     end
--- spec/lib/spotify_client_spec.rb
+++ spec/lib/spotify_client_spec.rb
@@ -4,26 +4,28 @@ require 'rails_helper'
 require 'spotify_client'
 
 describe SpotifyClient, :vcr do
-  describe '.get_playlist_by_id' do
+  subject(:client) { described_class.new }
+
+  describe '#get_playlist_by_id' do
     let(:playlist_id) { '3IlD894HSWDF8YlkCP25Sq' }
 
     it 'returns the correct playlist' do
-      expect(described_class.get_playlist_by_id(playlist_id).id).to eq playlist_id
+      expect(client.get_playlist_by_id(playlist_id).id).to eq playlist_id
     end
   end
 
-  describe '.get_artists_by_ids' do
+  describe '#get_artists_by_ids' do
     let(:artist_ids) { %w[1iNbNgr25BaS5e9a8xUZ9J 3ggwAqZD3lyT2sbovlmfQY] }
 
     it 'returns an array with the correct artists in it' do
-      expect(described_class.get_artists_by_ids(artist_ids).map(&:id)).to match_array artist_ids
+      expect(client.get_artists_by_ids(artist_ids).map(&:id)).to match_array artist_ids
     end
 
     context 'when it is called with too many artist_ids' do
       let(:too_many_artist_ids) { Array.new(51) { generate_spotify_id } }
 
       it 'raises an error' do
-        expect { described_class.get_artists_by_ids(too_many_artist_ids) }.to raise_error(ArgumentError)
+        expect { client.get_artists_by_ids(too_many_artist_ids) }.to raise_error(ArgumentError)
       end
     end
   end
