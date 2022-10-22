023d97a8e4b45fafc11254666ce9450c87c82f0a
m-gizzi <mgizzi@eligoenergy.com>
2022-10-22 15:24:43 -0500

    Add specs for batch jobs

---
+++ spec/sidekiq/batch_update_playlists_track_data_job_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe BatchUpdatePlaylistsTrackDataJob do
+  let(:playlist_ids) { Array.new(4) { generate_spotify_id } }
+  let(:playlist) { create(:playlist) }
+
+  before do
+    build_list(:playlist, 4, user: build(:user)).each_with_index do |playlist, i|
+      playlist.spotify_id = playlist_ids[i]
+      playlist.save!
+    end
+  end
+
+  describe '#perform' do
+    it 'enqueues an UpdatePlaylistTrackDataJob for each eligible Playlist' do
+      expect do
+        described_class.perform_async
+        described_class.drain
+      end.to change(UpdatePlaylistTrackDataJob.jobs, :size).by(4)
+    end
+  end
+end
