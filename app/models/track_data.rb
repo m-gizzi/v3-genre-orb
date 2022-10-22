8a41ce46cbaeae20497167b1bca9ca034ae6fd12
m-gizzi <mgizzi@eligoenergy.com>
2022-10-21 21:09:47 -0500

    Add scraping_status to TrackData

--- app/models/track_data.rb
+++ app/models/track_data.rb
@@ -3,4 +3,9 @@
 class TrackData < ApplicationRecord
   belongs_to :playlist
   has_and_belongs_to_many :tracks
+
+  enum scraping_status: {
+    in_progress: 'in_progress',
+    completed: 'completed'
+  }
 end
