03c09ba0dd4dfedbd1bc9570d1f590d091f8c2a4
m-gizzi <mgizzi@eligoenergy.com>
2022-10-27 22:20:56 -0500

    Make TrackData polymorphically associated to playlist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_28_025337) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_28_030500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,6 +85,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_28_025337) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.enum "scraping_status", default: "incomplete", null: false, enum_type: "scraping_status"
+    t.string "playlist_type", default: "Playlist", null: false
     t.index ["playlist_id"], name: "index_track_data_on_playlist_id"
   end
 
