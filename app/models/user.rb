7a2a9368514a0e04d7f39f1a32dbc3d4b8f181eb
m-gizzi <mgizzi@eligoenergy.com>
2022-10-28 14:26:25 -0500

    Add User#to_rspotify_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,18 @@ class User < ApplicationRecord
   validates :full_name, presence: true
   validates :spotify_id, presence: { message: I18n.t('active_record_validations.spotify_id.presence') },
                          uniqueness: { message: I18n.t('active_record_validations.spotify_id.uniqueness') }
+
+  def to_rspotify_user
+    raise 'User has not granted this app access to their Spotify account.' unless oauth_credential
+
+    RSpotify::User.new(
+      {
+        credentials: {
+          token: oauth_credential.decrypted_access_token,
+          refresh_token: oauth_credential.decrypted_refresh_token
+        },
+        info: { id: spotify_id }
+      }.with_indifferent_access
+    )
+  end
 end
