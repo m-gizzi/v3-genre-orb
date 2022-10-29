e08e3ce0c81bac85396e3a1e8fc6d5f069d86f5b
m-gizzi <mgizzi@eligoenergy.com>
2022-10-28 20:47:57 -0500

    DRY up code between the two services

--- lib/responses/get_tracks.rb
+++ lib/responses/get_tracks.rb
@@ -15,5 +15,29 @@ module Responses
     def calculate_next_offset
       limit + offset
     end
+
+    def import_tracks_and_artists!
+      items.map do |track_hash|
+        track_attributes = track_hash['track']
+        artist_attributes = track_attributes['artists']
+
+        track = find_or_create_track_from_attributes!(track_attributes)
+        track.artists = find_or_create_artists_from_attributes!(artist_attributes)
+
+        track
+      end
+    end
+
+    private
+
+    def find_or_create_track_from_attributes!(track_attributes)
+      Track.create_with(name: track_attributes['name']).find_or_create_by!(spotify_id: track_attributes['id'])
+    end
+
+    def find_or_create_artists_from_attributes!(artist_attributes)
+      artist_attributes.map do |attributes|
+        Artist.create_with(name: attributes['name']).find_or_create_by!(spotify_id: attributes['id'])
+      end
+    end
   end
 end
