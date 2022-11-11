fb9a9845f750c7ad17681e6a5c4fe393892484a3
m-gizzi <mgizzi@eligoenergy.com>
2022-11-10 21:40:36 -0600

    Update UpdatePlaylistTrackDataJob specs to handle both types of playlist

--- spec/sidekiq/update_playlist_track_data_job_spec.rb
+++ spec/sidekiq/update_playlist_track_data_job_spec.rb
@@ -3,16 +3,28 @@
 require 'rails_helper'
 
 describe UpdatePlaylistTrackDataJob, type: :job do
-  let(:playlist) { create(:playlist) }
-
   describe '#perform', :vcr do
-    it 'creates a new TrackData to log the scraped data' do
-      expect { described_class.perform_async(playlist.id, playlist.class.to_s) }.to change(TrackData, :count).by(1)
+    shared_examples 'UpdatePlaylistTrackDataJob examples' do
+      it 'creates a new TrackData to log the scraped data' do
+        expect { described_class.perform_async(playlist.id, playlist.class.to_s) }.to change(TrackData, :count).by(1)
+      end
+
+      it 'associates all the Tracks from the response to the new TrackData' do
+        described_class.perform_async(playlist.id, playlist.class.to_s)
+        expect(playlist.reload.current_track_data.tracks.count).to eq playlist.song_count
+      end
     end
 
-    it 'associates all the Tracks from the response to the new TrackData' do
-      described_class.perform_async(playlist.id, playlist.class.to_s)
-      expect(playlist.reload.current_track_data.tracks.count).to eq playlist.song_count
+    context 'when the playlist passed to the job is a Playlist' do
+      let(:playlist) { create(:playlist) }
+
+      include_examples 'UpdatePlaylistTrackDataJob examples'
+    end
+
+    context 'when the playlist passed to the job is a LikedSongsPlaylist' do
+      let(:playlist) { create(:liked_songs_playlist) }
+
+      include_examples 'UpdatePlaylistTrackDataJob examples'
     end
   end
 end
