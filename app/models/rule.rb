61921edfe244a9f198f3b537803b9bba5d4677b2
m-gizzi <mgizzi@eligoenergy.com>
2022-11-26 01:54:25 -0600

    Add Rule#tracks_that_pass_condition

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -4,6 +4,16 @@ class Rule < ApplicationRecord
   belongs_to :rule_group
 
   enum condition: {
-    genre: 'genre'
+    any_artists_genre: 'any_artists_genre',
+    all_artists_genre: 'all_artists_genre'
   }
+
+  def tracks_that_pass_condition
+    case condition
+    when 'any_artists_genre'
+      Track.with_at_least_one_artist_in_genre(value)
+    when 'all_artists_genre'
+      Track.with_all_artists_in_genre(value)
+    end
+  end
 end
