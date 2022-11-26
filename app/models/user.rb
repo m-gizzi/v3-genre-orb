0b07a3b65b11df694c04af7f70d73f5308d03e50
m-gizzi <mgizzi@eligoenergy.com>
2022-11-26 15:21:17 -0600

    Remove missed api_logs reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_one :oauth_credential, dependent: :destroy
   has_one :liked_songs_playlist, dependent: :destroy
   has_many :playlists, dependent: :destroy
-  has_many :api_logs, dependent: :destroy
 
   validates :full_name, presence: true
   validates :spotify_id, presence: { message: I18n.t('active_record_validations.spotify_id.presence') },
