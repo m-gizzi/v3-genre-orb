ab91dd6e57ddc822499f21a56431a1b332fe242d
m-gizzi <mgizzi@eligoenergy.com>
2022-12-11 18:08:50 -0600

    Refactor SpotifyClient

--- lib/spotify_client.rb
+++ lib/spotify_client.rb
@@ -3,13 +3,15 @@
 require 'responses/response'
 
 class SpotifyClient
+  attr_accessor :exception
+
   SPOTIFY_SIDEKIQ_QUEUES = [
     Sidekiq::Queue['spotify_api_calls'],
     Sidekiq::Queue['low_rate_spotify_api_calls']
   ].freeze
 
   def get_playlist_by_id(playlist_id)
-    handle_retryable_error(RestClient::TooManyRequests) do
+    handle_too_many_requests_error do
       # rubocop:disable Rails/DynamicFindBy
       RSpotify::Playlist.find_by_id(playlist_id)
       # rubocop:enable Rails/DynamicFindBy
@@ -22,26 +24,26 @@ class SpotifyClient
       raise ArgumentError, "#{__method__} cannot accept more than #{MAXIMUM_ARTIST_LOOKUPS_PER_CALL} ids as arguments"
     end
 
-    handle_retryable_error(RestClient::TooManyRequests) do
+    handle_too_many_requests_error do
       RSpotify::Artist.find(artist_ids)
     end
   end
 
   def get_related_artists(rspotify_artist)
-    handle_retryable_error(RestClient::TooManyRequests) do
+    handle_too_many_requests_error do
       rspotify_artist.related_artists
     end
   end
 
   def get_tracks(rspotify_playlist, offset: 0)
-    handle_retryable_error(RestClient::TooManyRequests) do
+    handle_too_many_requests_error do
       raw_response = rspotify_playlist.tracks(raw_response: true, offset:)
       Responses::GetTracks.new(raw_response)
     end
   end
 
   def get_liked_tracks(rspotify_user, offset: 0)
-    handle_retryable_error(RestClient::TooManyRequests) do
+    handle_too_many_requests_error do
       # RSpotify::User.saved_tracks has a maximum limit of 50 tracks returned
       raw_response = rspotify_user.saved_tracks(raw_response: true, limit: 50, offset:)
       Responses::GetTracks.new(raw_response)
@@ -50,10 +52,10 @@ class SpotifyClient
 
   private
 
-  def handle_retryable_error(rescued_exception_classes)
+  def handle_too_many_requests_error
     yield
-  rescue *rescued_exception_classes => e
-    @error = e
+  rescue RestClient::TooManyRequests => e
+    self.exception = e
     pause_queues
     log_error
     sleep(seconds_to_retry_after)
@@ -66,7 +68,7 @@ class SpotifyClient
 
   def error_retry_after_header
     # Spotify returns a header that tells you the number of seconds to wait before trying again
-    @error.http_headers[:retry_after]
+    exception.http_headers[:retry_after]
   end
 
   def pause_queues
@@ -74,6 +76,6 @@ class SpotifyClient
   end
 
   def log_error
-    Bugsnag.notify(@error) { |event| event.add_metadata(:diagnostics, { headers: @error.http_headers }) }
+    Bugsnag.notify(exception) { |event| event.add_metadata(:diagnostics, { headers: exception.http_headers }) }
   end
 end
