867e2b9f7ed5928b69401a2fc617a0e3af19c705
m-gizzi <mgizzi@eligoenergy.com>
2022-12-25 17:47:38 -0600

    Add validations to PlaylistsCurrentTrackDataImport

--- app/models/playlists_current_track_data_import.rb
+++ app/models/playlists_current_track_data_import.rb
@@ -5,4 +5,14 @@ class PlaylistsCurrentTrackDataImport < ApplicationRecord
   belongs_to :track_data_import
 
   validates :playlist_id, uniqueness: { scope: :playlist_type }
+  validate :track_data_import_is_complete
+  validate :track_data_import_belongs_to_playlist
+
+  def track_data_import_is_complete
+    errors.add(:track_data_import_scraping_status, 'must_be_completed') unless track_data_import.completed?
+  end
+
+  def track_data_import_belongs_to_playlist
+    errors.add(:track_data_import, 'must_belong_to_playlist') unless track_data_import.playlist == playlist
+  end
 end
