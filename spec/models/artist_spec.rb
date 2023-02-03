9aee5feea6e666ac43fa39c09c6e1fcf18280d5b
m-gizzi <mgizzi@eligoenergy.com>
2023-02-02 23:00:34 -0600

    Change vcr to stub for .to_rspotify_artists

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -95,57 +95,59 @@ describe Artist, type: :model do
     end
   end
 
-  describe '.to_rspotify_artists', :vcr do
-    let(:artist_ids) do
-      %w[
-        6GI52t8N5F02MxU0g5U69P
-        36hwOoNPgnsKnhoMBYpJrJ
-        69VgCcXFV59QuQWEXSTxfK
-        12LgviUQ9DbfYJJ9niDWRq
-        3nXMZsj1H0F7h9SDUeYNjg
-      ]
-    end
+  describe '.to_rspotify_artists' do
+    let(:get_five_artists_body) { File.open('./spec/fixtures/successful_get_five_artists.json') }
+    let(:number_of_artists) { 5 }
 
     before do
-      artist_ids.each do |id|
-        create(:artist, spotify_id: id)
-      end
+      create_list(:artist, number_of_artists)
+
+      stub_request(:get, %r{/api.spotify.com/v1/artists}).with do |request|
+        request.uri.query_values['ids'].split(',').count == 5
+      end.to_return(
+        status: 200,
+        body: get_five_artists_body
+      )
     end
 
-    it 'returns the right artists from Spotify' do
-      expect(described_class.to_rspotify_artists.map(&:id)).to match_array(artist_ids)
+    it 'returns an array of RSpotify::Artists' do
+      expect(described_class.to_rspotify_artists).to all(be_a RSpotify::Artist)
+    end
+
+    it 'returns the correct number of RSpotify::Artists' do
+      expect(described_class.to_rspotify_artists.count).to eq number_of_artists
     end
 
     context 'when the method is called on a large number of Artists' do
-      let(:artist_ids) do
-        %w[
-          3qb5O9pLE0urqttdq4CqLS 0K6DXvfMXmF7L4h0P7Ivva 5j2d5CS0sh2LTpFsrKAFcW 2VcT3LFa9zgz333gKf44jM
-          0vXLxVov9wThTze1YI6rIU 0v3A74qYtkL8Wjj1rzLe9Z 2vbPZ0xMZiXxUkR744bcoh 3iexGtoBAyCUbxOKeru5py
-          5slSOLb4CcoPOgptg4tsxo 4qzeNR1Pjrgu55xYR9foVa 2AqJPIURzQPk72FXeh8LJx 0SsM5OCOiiwycZIO63OT1S
-          5jX8L7D6vWHYlNj9k361vI 4L5LykGl5CSopyhoOTP2xb 58PU6MWEroVyXXTXmpzdny 0SEjW4NsEIkiSgpoxLuQCI
-          74wzAUjm5ENI8EtWCaEBDz 2d4DsLfaKstHsimzDw0LyM 7HY8lCkyEUSL5jfENlkshV 4js8BDiQwnHLlDmT1shPH7
-          5h6tKZX6V9AYYRbvsxZakW 75azd12Swc8yTuOY5lhjMU 3qznYzY3OEo9gewAVrGFHr 0X4yScuctoizdQapaKybMS
-          4rSwFTxoQwxwKjeQlSiLP7 7uLXW75DlTRahz2WKJZGeO 2trnx6921S62OeomGmiG01 3xMGQzg5JvqwOU7oaVuKwJ
-          2qVuzGuGrGVA0JXOphHn3F 2GOM1BVtYZRNen3IkfOrRJ 4n3WEY09NL7X6zTWNP4XTC 3beAmqA2s5xwxDAIFJwDG9
-          5VAsNbws6j873uHN1VKEmZ 6Ej6u8fvyHFFKn5GaQ5cPn 7djT11AqtEBzdME1OGFfxL 2Azc5aV2MqKRb6S3CENEZl
-          6YZ98HsiE3oCtQB0E99Qtt 5w09r1HW3Dc8BMYIvqbnnF 63JN4A4IroUvAN1fyWmsHB 0t9i2yNpYr4QGde2gz8YVg
-          161AC1AVRkIGIMxyj5djFQ 6eA5XwY5JNBLqm3X8u2AqD 72vSlNssmiyjzCAt4Z61ME 0b05DhI4gD2fNDlqbFBOyt
-          2rGVkwUEaofowUjyWnQMXG 3fxdn6mfKvNFJ1Zx37On7W 7wDfZhaCORLgP3K62R3MJK 1I7FVmvisCtSFzmm87mbLR
-          6hCsqVHnBo1BVQWuIjRMkL 1BMpbT4eEAJreNQKAdPBiH 31ufWigY0fDQ8p8EJxvaGH 2imgRP2nURSgnz95gLshPF
-          0u0iaH7ESuQHoYY06AButU 1D2acYwVzm55CayJwUwqB5 7wkjhjRAaChOE0hMghtlvc 3VvmUsYPzFheK2wJGKcXxp
-          0LUkZYSaO60vSGVxgZM5PI 3MUnAJMKx97SfTZYNB2iXY 3qQgY6bwLUYIkxPWTaafHR 7pfZnHTXD8eEUVQdDfPKwN
-          7jfErUM4CHGxIaqCEgkxJm 0JC4o7RAl3GNs1MbThyuoD 04dvqs3z04paTr2YeAmpe0 3mp5zzEz1qTq7X5a2yOS2B
-          5sM4cZ9MHihs0tbmkiE9n8 3jT2Zl57kYDK5qI8fkl1L9 1YumFgivFXVVg1AKBJKE5e 67CxTr6NMaF4v8X8rxXFIA
-          7taR4NVoGObH3v6708KBgV 41kO57U2yrvAWlng1p5g6g 6qokeohfvbaNK1m0VfIEtB
-        ]
+      let(:get_ten_artists_body) { File.open('./spec/fixtures/successful_get_ten_artists.json') }
+      let(:get_fifty_artists_body) { File.open('./spec/fixtures/successful_get_fifty_artists.json') }
+      let(:number_of_artists) { 60 }
+
+      before do
+        stub_request(:get, %r{/api.spotify.com/v1/artists}).with do |request|
+          request.uri.query_values['ids'].split(',').count == 50
+        end.to_return(
+          status: 200,
+          body: get_fifty_artists_body
+        )
+        stub_request(:get, %r{/api.spotify.com/v1/artists}).with do |request|
+          request.uri.query_values['ids'].split(',').count == 10
+        end.to_return(
+          status: 200,
+          body: get_ten_artists_body
+        )
       end
 
       it 'does not raise an error' do
         expect { described_class.to_rspotify_artists }.not_to raise_error
       end
 
-      it 'returns the right artists from Spotify' do
-        expect(described_class.to_rspotify_artists.map(&:id)).to match_array(artist_ids)
+      it 'returns an array of RSpotify::Artists' do
+        expect(described_class.to_rspotify_artists).to all(be_a RSpotify::Artist)
+      end
+
+      it 'returns the correct number of RSpotify::Artists' do
+        expect(described_class.to_rspotify_artists.count).to eq number_of_artists
       end
     end
   end
