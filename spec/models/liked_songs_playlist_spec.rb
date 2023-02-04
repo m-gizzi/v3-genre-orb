37dbd9807d85fff916bc6bd647a1d08d1475d12a
m-gizzi <mgizzi@eligoenergy.com>
2023-02-03 22:23:59 -0600

    Stub LikedSongsPlaylist specs

--- spec/models/liked_songs_playlist_spec.rb
+++ spec/models/liked_songs_playlist_spec.rb
@@ -8,8 +8,13 @@ describe LikedSongsPlaylist, type: :model do
 
   let(:user) { playlist.user }
 
+  before do
+    stub_request(:get, %r{https://api.spotify.com/v1/me/tracks})
+      .to_return(status: 200, body: File.read('spec/fixtures/successful_get_liked_tracks.json'))
+  end
+
   describe '#sync_with_spotify!' do
-    it 'updates the song_count of the playlist', :vcr do
+    it 'updates the song_count of the playlist' do
       response = SpotifyClient.new.get_liked_tracks(user.to_rspotify_user)
       expect { playlist.sync_with_spotify!(response) }.to(change(playlist, :song_count))
     end
