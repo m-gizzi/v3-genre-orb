0f5f71dcb59a36bbe3e7bddd05685f02f8751417
m-gizzi <mgizzi@eligoenergy.com>
2023-02-19 22:21:00 -0600

    Get #remove_tracks_from_playlist! specs passing

--- spec/lib/spotify_client_spec.rb
+++ spec/lib/spotify_client_spec.rb
@@ -221,12 +221,46 @@ describe SpotifyClient do
     end
   end
 
-  # describe '#remove_tracks_from_playlist!' do
-  #   let(:playlist) { create(:playlist) }
-  #   let(:rspotify_playlist) { playlist.to_rspotify_playlist }
+  describe '#remove_tracks_from_playlist!' do
+    let(:playlist) { create(:playlist, user:) }
+    let(:user) { create(:user, :with_spotify_tokens, spotify_id: '31upmxyqdkt5utuji6r5wsrkgn4e') }
+    let(:rspotify_playlist) { playlist.to_rspotify_playlist }
+    let(:track_uris) { Track.all.map(&:spotify_uri) }
+    let!(:target_stub) do
+      stub_request(:delete, "https://api.spotify.com/v1/users/#{user.spotify_id}/playlists/3nwz2mVTVbWSGMSFMzN7pu/tracks")
+        .to_return(status: 201, body: { snapshot_id: generate_spotify_id }.to_json)
+    end
 
-  #   it 'removes the tracks from the playlist' do
-  #     client.remove_tracks_from_playlist!(rspotify_playlist, track_uris)
-  #   end
-  # end
+    before do
+      create_list(:track, 5)
+
+      playlist.user.to_rspotify_user # This needs to be called in order to authorize this API call
+
+      stub_request(:get, "https://api.spotify.com/v1/playlists/#{playlist.spotify_id}")
+        .to_return(status: 200, body: File.read('spec/fixtures/successful_get_playlist.json'))
+    end
+
+    it 'makes a call to Spotify to remove the tracks from the playlist' do
+      client.remove_tracks_from_playlist!(rspotify_playlist, track_uris)
+      expect(target_stub).to have_been_requested
+    end
+
+    context 'when a 429 error is raised' do
+      include_context 'with a stubbed retryable error' do
+        let(:error_class) { RestClient::TooManyRequests }
+        let(:recipient) { rspotify_playlist }
+        let(:message) { :remove_tracks! }
+      end
+
+      it 'reattempts the call that errored out' do
+        client.remove_tracks_from_playlist!(rspotify_playlist, track_uris)
+        expect(rspotify_playlist).to have_received(:remove_tracks!).twice
+      end
+
+      it 'makes a call to Spotify to remove the tracks from the playlist' do
+        client.remove_tracks_from_playlist!(rspotify_playlist, track_uris)
+        expect(target_stub).to have_been_requested
+      end
+    end
+  end
 end
