e5bc95f42bdcace8c5302bd492ee4f583a63d4db
m-gizzi <mgizzi@eligoenergy.com>
2023-02-23 22:51:32 -0600

    RemoveTracksFromPlaylist Job and Service

---
+++ app/services/remove_tracks_from_playlist_service.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class RemoveTracksFromPlaylistService < ApplicationService
+  include HasSpotifyClient
+
+  attr_reader :playlist, :track_uris
+
+  def initialize(playlist, track_uris)
+    @playlist = playlist
+    @track_uris = track_uris
+  end
+
+  def call
+    rspotify_playlist = playlist.to_rspotify_playlist
+    spotify_client.remove_tracks_from_playlist!(rspotify_playlist, track_uris)
+  end
+end
