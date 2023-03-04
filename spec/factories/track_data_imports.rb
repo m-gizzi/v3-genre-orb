cf2e866da1549cc5268b481354138f9e63acf6b1
m-gizzi <mgizzi@eligoenergy.com>
2023-03-03 18:54:19 -0600

    Add :current_track_data to track_data_import factory

--- spec/factories/track_data_imports.rb
+++ spec/factories/track_data_imports.rb
@@ -4,5 +4,11 @@ FactoryBot.define do
   factory :track_data_import do
     scraping_status { 'completed' }
     playlist { create(:playlist, spotify_id: generate_spotify_id) }
+
+    trait :current_track_data do
+      after(:create) do |track_data_import|
+        track_data_import.playlist.current_track_data = track_data_import
+      end
+    end
   end
 end
