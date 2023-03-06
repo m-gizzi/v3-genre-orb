eac3c996e80064a1a225d4e039e5bc638e5b6102
m-gizzi <mgizzi@eligoenergy.com>
2023-03-05 22:14:05 -0600

    Add uniqueness validation to Rule#value

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -3,6 +3,8 @@
 class Rule < ApplicationRecord
   belongs_to :rule_group
 
+  validates :value, uniqueness: { scope: :rule_group_id }
+
   enum condition: {
     any_artists_genre: 'any_artists_genre',
     all_artists_genre: 'all_artists_genre'
